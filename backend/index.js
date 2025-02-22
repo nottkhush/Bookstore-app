03cb104bfacd0175a21035a6182a367370630880
Khushal Jain <143007493+nottkhush@users.noreply.github.com>
2025-02-23 01:34:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // Option 2: Allow custom origins
 app.use(
     cors({
-        origin: '',
+        origin: ['https://bookstore-app-frontend-mu.vercel.app/'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type']
     })
