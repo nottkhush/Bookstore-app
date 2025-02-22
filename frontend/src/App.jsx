b41fd5614410a792a00b7c7ffb61b657d7b067a9
Khushal Jain <khush6569@gmail.com>
2025-02-23 01:27:28 +0530

    final push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,11 +10,11 @@ function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBooks />} />
-      <Route path='/books/details/:id' element={<ShowBooks />} />
-      <Route path='/books/edit/:id' element={<EditBooks />} />
-      <Route path='/books/delete/:id' element={<DeleteBooks />} />
+      <Route path='bookstore-app-ivory.vercel.app/' element={<Home />} />
+      <Route path='bookstore-app-ivory.vercel.app/books/create' element={<CreateBooks />} />
+      <Route path='bookstore-app-ivory.vercel.app/books/details/:id' element={<ShowBooks />} />
+      <Route path='bookstore-app-ivory.vercel.app/books/edit/:id' element={<EditBooks />} />
+      <Route path='bookstore-app-ivory.vercel.app/books/delete/:id' element={<DeleteBooks />} />
     </Routes>
   )
 }
